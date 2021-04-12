4d744eb84b467aacd6ad75ddb472e3e4d60786a4
MatheusAntonioF <=>
2021-04-12 17:49:24 -0300

    [FEAT] Refactor specifications

--- src/modules/cars/useCases/createSpecifications/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecifications/CreateSpecificationController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import { CreateSpecificationUseCase } from './CreateSpecificationUseCase';
 
 class CreateSpecificationController {
-  constructor(private createSpecificationUseCase: CreateSpecificationUseCase) {}
-
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
 
-    this.createSpecificationUseCase.execute({ name, description });
+    container
+      .resolve(CreateSpecificationUseCase)
+      .execute({ name, description });
 
     return response.status(201).send();
   }
--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
-
-  handle(request: Request, response: Response): Response {
-    const all = this.listCategoriesUseCase.execute();
+  async handle(request: Request, response: Response): Promise<Response> {
+    const all = await container.resolve(ListCategoriesUseCase).execute();
 
     return response.json(all);
   }
