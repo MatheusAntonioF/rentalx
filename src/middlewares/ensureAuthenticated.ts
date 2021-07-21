f0e04ef7856de225c8758403b56b5c3e94895e24
Matheus Antonio <matheusantoniof@gmail.com>
2021-07-21 00:40:15 -0300

    [FEAT] Exceptions

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import { AppError } from '../errors/AppError';
 import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
 
 interface IPayload {
@@ -14,7 +15,7 @@ export async function ensureAuthenticated(
 ): Promise<void> {
   const authHeader = request.headers.authorization;
 
-  if (!authHeader) throw new Error('Token missing');
+  if (!authHeader) throw new AppError('Token missing', 401);
 
   const [, token] = authHeader.split(' ');
 
@@ -28,10 +29,10 @@ export async function ensureAuthenticated(
 
     const user = await usersRepository.findById(user_id);
 
-    if (!user) throw new Error('User doesnt exists');
+    if (!user) throw new AppError('User doesnt exists', 401);
 
     next();
   } catch (err) {
-    throw new Error('Invalid Token');
+    throw new AppError('Invalid Token', 401);
   }
 }
