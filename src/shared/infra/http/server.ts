38fa82428736780c86eb0653609f25ffade7666c
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2022-05-12 14:35:41 -0300

    feat: create infra directory

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,12 +3,11 @@ import express, { NextFunction } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import { AppError } from './errors/AppError';
+import swaggerFile from '../../../swagger.json';
+import { AppError } from '../../errors/AppError';
 import { router } from './routes';
-import swaggerFile from './swagger.json';
-
-import './database';
-import './shared/container';
+import '../typeorm';
+import '../../container';
 
 const app = express();
 
