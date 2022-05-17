831c7b9a6b072b1597721385bfe0536ba9c960c2
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2022-05-17 11:15:23 -0300

    feat: create test to rental cars

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,53 @@
+import { IDateProvider } from '../../../../shared/container/providers/DateProvider/IDateProvider';
+import { AppError } from '../../../../shared/errors/AppError';
+import { Rental } from '../../infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '../../repositories/IRentalsRepository';
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider
+  ) {}
+
+  async execute(data: IRequest): Promise<Rental> {
+    const minimumHour = 24;
+
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      data.car_id
+    );
+
+    if (carUnavailable) throw new AppError('Car is unavailable');
+
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      data.user_id
+    );
+
+    if (rentalOpenToUser)
+      throw new AppError('There is a rental in progress for user');
+
+    const dateNow = this.dateProvider.dateNow();
+
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      data.expected_return_date
+    );
+
+    if (compare < minimumHour) throw new AppError('Invalid return time');
+
+    const rental = await this.rentalsRepository.create({
+      user_id: data.user_id,
+      car_id: data.car_id,
+      expected_return_date: data.expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
