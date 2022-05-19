2a4c565f8df3d850c2dfe56ea1a1814d887f9e0d
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2022-05-18 23:20:59 -0300

    feat: resource to send mail when user forgot your password

--- src/shared/container/providers/MailProvider/IMailProvider.ts
+++ src/shared/container/providers/MailProvider/IMailProvider.ts
@@ -1,5 +1,10 @@
 interface IMailProvider {
-  sendMail(to: string, subject: string, body: string): Promise<void>;
+  sendMail(
+    to: string,
+    subject: string,
+    variables: Record<string, unknown>,
+    path: string
+  ): Promise<void>;
 }
 
 export { IMailProvider };
