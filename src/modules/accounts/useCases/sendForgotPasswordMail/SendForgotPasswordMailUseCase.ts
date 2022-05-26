64fc69a50122ab9fe722f7caf69efe61b5bc6853
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2022-05-26 01:06:21 -0300

    feat: create provider to send mail using aws ses

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -15,7 +15,7 @@ class SendForgotPasswordMailUseCase {
     @inject('UsersTokensRepository')
     private usersTokensRepository: IUsersTokensRepository,
     @inject('DateProvider') private dateProvider: IDateProvider,
-    @inject('EtherealMailProvider') private mailProvider: IMailProvider
+    @inject('MailProvider') private mailProvider: IMailProvider
   ) {}
 
   async execute(email: string): Promise<void> {
