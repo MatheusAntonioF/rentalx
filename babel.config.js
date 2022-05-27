7ae5f41616f606a879e067982dab2dccbd6138dc
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2022-05-27 20:09:29 -0300

    feat: add babel to build projet

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    // here we can pass a plugin to manage root imports using @ for example - using  babel module resolver
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
